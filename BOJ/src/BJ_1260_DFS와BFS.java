186c19a22c9ae03079103e3c0fefd0e31c92d1dc
kian6365 <kian6365@naver.com>
2022-02-21 19:18:52 +0900

    0221

--- BOJ/src/BJ_1260_DFS와BFS.java
+++ BOJ/src/BJ_1260_DFS와BFS.java
@@ -15,13 +15,12 @@ public class BJ_1260_DFS와BFS {
 		M = Integer.parseInt(splited[1]);
 		V = Integer.parseInt(splited[2]);
 		
-		int[][] adjMatrix = new int[N][N];	// 정점 수 크기로 생성
+		int[][] adjMatrix = new int[N][N];
 		
 		for(int i = 0; i < M; i++) {
 			splited = in.readLine().split(" ");
 			int from = Integer.parseInt(splited[0]) - 1;
 			int to = Integer.parseInt(splited[1]) - 1;
-			// 무향이기 때문에 간선 하나로 양방향 모두 처리
 			adjMatrix[to][from] = adjMatrix[from][to] = 1;
 		}
 		
@@ -34,7 +33,6 @@ public class BJ_1260_DFS와BFS {
 		visited[current] = true;
 		System.out.print(current + 1 + " ");
 		
-		// current 정점의 인접정점 처리(단, 방문하지 않은 인접정점만)
 		for(int j = 0; j < N; j++) {
 			if(!visited[j] && adjMatrix[current][j] != 0) {
 				dfs(adjMatrix, visited, j);
@@ -53,9 +51,8 @@ public class BJ_1260_DFS와BFS {
 			int current = queue.poll();
 			System.out.print(current + 1 + " ");
 			
-			// current 점정의 인접 정점 처리(단, 방문하지 않은 인접정점만)
 			for(int j = 0; j < N; j++) {
-				if(!visited[j] && adjMatrix[current][j] != 0) {	// 방문하지 않았고, current에서 갈수 있다면
+				if(!visited[j] && adjMatrix[current][j] != 0) {
 					queue.offer(j);
 					visited[j] = true;
 				}
