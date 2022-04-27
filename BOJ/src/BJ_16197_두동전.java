217427ccf2b17e523d8d8294efdc63fe0a92552b
GiYeongLee <kian6365@naver.com>
2022-04-27 22:26:52 +0900

    0427

---
+++ BOJ/src/BJ_16197_두동전.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ_16197_두동전 {
+	
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
