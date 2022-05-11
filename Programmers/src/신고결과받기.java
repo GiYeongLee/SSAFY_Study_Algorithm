8241f8f3066535b70f05410de3067e44cc65da2e
GiYeongLee <kian6365@naver.com>
2022-05-11 23:57:07 +0900

    0511

--- Programmers/src/신고결과받기.java
+++ Programmers/src/신고결과받기.java
@@ -2,16 +2,13 @@ import java.util.*;
 
 public class 신고결과받기 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		String[] id_list = {"muzi", "frodo", "apeach", "neo"};
 		String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
 		int k = 2;
 		
-		int[] answer = solution(id_list, report, k);
-		
+		int[] answer = solution(id_list, report, k);	
 		System.out.println(Arrays.toString(answer));
-
 	}
 	
 	static class User{
