a43a83578ac901625ac9a8ed741d2f328bfc07e4
GiYeongLee <kian6365@naver.com>
2022-07-22 09:43:50 +0900

    solved : Programmers 괄호변환

--- Programmers/src/괄호변환.java
+++ Programmers/src/괄호변환.java
@@ -1,87 +1,86 @@
+import java.util.*;
+
 public class 괄호변환 {
     public static void main(String[] args) {
-        String p = ")(";
+        String p = "(()))(";
         System.out.println(solution(p));
     }
 
     public static String solution(String p) {
-        if (check(p)) {
-            return p;
-        }
+        if (check(p)) return p;
         return func(p);
     }
     
     public static String func(String p) {
         // step 1
-        if (p.length() == 0) {
-            return p;
-        }
+        if (p.length() == 0) return p;
 
-        // spep 2
+        // step 2
         int leftCount = 0;
         int rightCount = 0;
-
-        String leftTemp = "";
-        String rightTemp = "";
+        String leftString = "";
+        String rightString = "";
 
         for (int i = 0; i < p.length(); i++) {
-            if (p.charAt(i) == '(') {
-                leftCount++;
-            } else {
-                rightCount++;
-            }
+            if (p.charAt(i) == '(') leftCount++;
+            else rightCount++;
 
             if (leftCount == rightCount) {
-                leftTemp = p.substring(0, i + 1);
-                rightTemp = p.substring(i + 1, p.length());
+                leftString = p.substring(0, i + 1);
+                rightString = p.substring(i + 1, p.length());
                 break;
             }
         }
 
         // step 3
-        if (check(leftTemp)) {
-            return leftTemp + func(rightTemp);
-        }
+        if (check(leftString)) return leftString + func(rightString);
         
         // step 4
         else {
             // step 4-1 ~ 4-3
-            rightTemp = "(" + func(rightTemp) + ")";
+            rightString = "(" + func(rightString) + ")";
 
             // step 4-4
-            leftTemp = leftTemp.substring(1, leftTemp.length() - 1);
+            leftString = leftString.substring(1, leftString.length() - 1);
 
-            for (int i = 0; i < leftTemp.length(); i++) {
-                switch (leftTemp.charAt(i)) {
+            for (int i = 0; i < leftString.length(); i++) {
+                switch(leftString.charAt(i)) {
                     case '(':
-                        rightTemp += ")";
+                        rightString += ")";
                         break;
                     case ')':
-                        rightTemp += "(";
+                        rightString += "(";
                         break;
                 }
             }
 
             // step 5
-            return rightTemp;
+            return rightString;
         }
 
     }
 
     public static boolean check(String p) {
-        int count = 0;
+        Stack<Character> stack = new Stack<Character>();
+
         for (int i = 0; i < p.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(p.charAt(i));
+                continue;
+            }
+
             switch (p.charAt(i)) {
                 case '(':
-                    count++;
+                    if (stack.peek() == ')') stack.pop();
+                    else stack.push(p.charAt(i));
                     break;
                 case ')':
-                    count--;
-                    if (count < 0) {
-                        return false;
-                    }
+                    if (stack.peek() == ')') stack.pop();
+                    else stack.push(p.charAt(i));
+                    break;
             }
         }
+        if(stack.size() > 0) return false;
         return true;
     }
 }
